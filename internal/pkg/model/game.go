7bdac8d6900458599972a45cb6738b0d75ec3bca
Nikita V <qask_platform@mail.ru>
2023-10-06 23:25:45 +0300

    [v1.0.22] Game method MarshalJSON() added

--- internal/pkg/model/game.go
+++ internal/pkg/model/game.go
@@ -1,9 +1,11 @@
 package model
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/mono83/maybe"
+	maybejson "github.com/mono83/maybe/json"
 )
 
 // Game ...
@@ -20,4 +22,36 @@ type Game struct {
 	PaymentType maybe.Maybe[string]
 	MaxPlayers  uint32
 	IsInMaster  bool
+}
+
+// MarshalJSON ...
+func (g Game) MarshalJSON() ([]byte, error) {
+	type wrapperGame struct {
+		ID          int32
+		ExternalID  maybejson.Maybe[int32]
+		LeagueID    int32
+		Type        int32
+		Number      string
+		Name        maybejson.Maybe[string]
+		PlaceID     int32
+		DateTime    time.Time
+		Price       uint32
+		PaymentType maybejson.Maybe[string]
+		MaxPlayers  uint32
+	}
+
+	wg := wrapperGame{
+		ID:          g.ID,
+		ExternalID:  maybejson.Wrap(g.ExternalID),
+		LeagueID:    g.LeagueID,
+		Type:        g.Type,
+		Number:      g.Number,
+		Name:        maybejson.Wrap(g.Name),
+		PlaceID:     g.PlaceID,
+		DateTime:    g.DateTime,
+		Price:       g.Price,
+		PaymentType: maybejson.Wrap(g.PaymentType),
+		MaxPlayers:  g.MaxPlayers,
+	}
+	return json.Marshal(wg)
 }
